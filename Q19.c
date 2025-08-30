733429ff962fb85264a32c6288cc1cc3b7dcb59c
Drishti Kapoor <kdrishti2610@gmail.com>
2025-08-30 12:16:23 +0530

    Update Q19.c

--- Q19.c
+++ Q19.c
@@ -3,7 +3,7 @@
 #include <stdio.h>
 int main() {
     float side1, side2, side3;
-    printf("Enter lengths of the three sides of the triangle: ");
+    // printf("Enter lengths of the three sides of the triangle: ");
     scanf("%f %f %f", &side1, &side2, &side3);
     if (side1 == side2 && side2 == side3) {
         printf("Equilateral.\n");
