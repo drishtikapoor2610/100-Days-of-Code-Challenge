e6f8e7eec6ff3ab98e9ce0a1eab4ca48923c3483
Drishti Kapoor <kdrishti2610@gmail.com>
2025-10-18 12:42:21 +0530

    Update Q105.c

--- Q105.c
+++ Q105.c
@@ -1,3 +1,4 @@
+
 /*
 Write a program to take an integer array nums of size n, and print the majority element. The majority element is the element that appears strictly more than ⌊n / 2⌋ times. Print -1 if no such element exists. Note: Majority Element is not necessarily the element that is present most number of times.
 Input 1:
@@ -22,8 +23,14 @@ Follow-up (optional): Can you do it in O(n) Time Complexity?
 */
 #include <stdio.h>
 int main(){
-    int nums[] = {2,2,1,1,1,2,2};
-    int n = sizeof(nums)/sizeof(nums[0]);
+   
+    int n;
+    scanf("%d", &n);
+    int nums[n];
+    for(int i = 0; i < n; i++){
+        scanf("%d", &nums[i]);
+    }
+    
     int count = 0;
     int candidate = -1;
 
